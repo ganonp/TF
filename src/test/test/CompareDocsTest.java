b169b89dd0388321f4c7a239392b53fca9ac06d7
Ganon <ganonp@gmail.com>
2017-04-19 12:02:41 -0700

    Renamed some more variables

--- src/test/test/CompareDocsTest.java
+++ src/test/test/CompareDocsTest.java
@@ -15,28 +15,28 @@ public class CompareDocsTest {
     public void runProgram(){
         CompareDocs test1 = new CompareDocs();
         test1.docDirectory = TestSuite.directory;
-        test1.words = new String[]{"bobs", "blobs", "hamburgers"};
+        test1.tfWordSet = new String[]{"bobs", "blobs", "hamburgers"};
         test1.runProgram();
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("bobs").getRatio("bobs") == 8d/15d);
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("bobs").docName.equals("8bobs4logs3blobs"));
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("blobs").getRatio("blobs") == 3d/15d);
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("blobs").docName.equals("8bobs4logs3blobs"));
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("hamburgers").getRatio("hamburgers") == 10d/20d);
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("hamburgers").docName.equals("5bobs5dogs10hamburgers"));
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("bobs").getRatio("bobs") == 8d/15d);
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("bobs").docName.equals("8bobs4logs3blobs"));
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("blobs").getRatio("blobs") == 3d/15d);
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("blobs").docName.equals("8bobs4logs3blobs"));
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("hamburgers").getRatio("hamburgers") == 10d/20d);
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("hamburgers").docName.equals("5bobs5dogs10hamburgers"));
     }
 
     @Test
     public void runProgram2(){
         CompareDocs test1 = new CompareDocs();
         test1.docDirectory = TestSuite.directory;
-        test1.words = new String[]{"archers", "lanas", "mallorys"};
+        test1.tfWordSet = new String[]{"archers", "lanas", "mallorys"};
         test1.runProgram();
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("archers").getRatio("archers") == 2000d/1000000d);
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("archers").docName.equals("2000archers1000000words"));
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("lanas").getRatio("lanas") == 1000d/1000000d);
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("lanas").docName.equals("1000lanas1000000words"));
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("mallorys").getRatio("mallorys") == 10000d/1000000d);
-        Assert.assertTrue(test1.maxTermFrequencyByDocMap.get("mallorys").docName.equals("10000mallorys1000000words"));
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("archers").getRatio("archers") == 2000d/1000000d);
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("archers").docName.equals("2000archers1000000words"));
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("lanas").getRatio("lanas") == 1000d/1000000d);
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("lanas").docName.equals("1000lanas1000000words"));
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("mallorys").getRatio("mallorys") == 10000d/1000000d);
+        Assert.assertTrue(test1.maxTermFrequencyByWordMap.get("mallorys").docName.equals("10000mallorys1000000words"));
     }
 
     @Before
